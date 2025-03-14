99b99f42dd199456b69f823b4654c15a2e978df3
yymato <070913a@gmail.com>
2025-03-14 18:04:45 +0300

    Первые две задачи

---
+++ main.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, make_response, session, render_template, redirect
+from flask_login import LoginManager, login_user, login_required, logout_user
+from data import db_session
+from data.users import User
+from forms.login_form import LoginForm
+from forms.register import RegisterForm
+
+app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+def main():
+    app.run()
+
+
+@app.route("/session_test")
+def session_test():
+    visits_count = session.get('visits_count', 0)
+    session['visits_count'] = visits_count + 1
+    return make_response(f"Вы пришли на эту страницу {visits_count + 1} раз")
+
+
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('register.html', form=form)
+@app.route('/')
+def index():
+    return redirect('/register')
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
